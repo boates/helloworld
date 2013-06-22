c48ae5ffaa9147d41144f14df48f05d9bf596a7a
Brian Boates <boates@squareup.com>
2013-06-22 14:22:09 -0700

    removed punctuation, modified foo to return square rather than x2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 def foo(x):
-    return x*2
+    """
+    foo returns x**2
+    """
+    return x**2
 
-def main(): 
-    print "Hello world!"
+def main():
+    print "Hello world"
 
 if __name__ == '__main__':
     main()
